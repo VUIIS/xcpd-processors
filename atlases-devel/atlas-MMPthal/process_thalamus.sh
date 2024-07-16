4a5cdd9a30142c9d5e75bd89f347dcf6408393c3
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-07-16 07:52:09 -0500

    redo

--- atlases-devel/atlas-MMPthal/process_thalamus.sh
+++ atlases-devel/atlas-MMPthal/process_thalamus.sh
@@ -4,23 +4,23 @@
 fslmaths /usr/local/fsl/data/atlases/Thalamus/Thalamus-maxprob-thr25-1mm.nii.gz -bin thalmask
 
 # Resample to match MMP atlas (1.5mm)
-flirt \
-    -in thalmask \
-    -ref space-MNI152NLin2009cAsym_atlas-MMP_dseg \
-    -usesqform \
-    -applyxfm \
-    -interp nearestneighbour \
-    -out rthalmask
+#flirt \
+#    -in thalmask \
+#    -ref space-MNI152NLin2009cAsym_atlas-MMP_dseg \
+#    -usesqform \
+#    -applyxfm \
+#    -interp nearestneighbour \
+#    -out rthalmask
 
 # We have 8464 voxels in the maxprob25 thalamus mask
-fslstats -K rthalmask rthalmask -m -v
+#fslstats -K rthalmask rthalmask -m -v
 
-./make_thalamus_voxels.py
+#./make_thalamus_voxels.py
 
-fslmaths \
-    space-MNI152NLin2009cAsym_atlas-MMP_dseg \
-    -add rthalatlas \
-    space-MNI152NLin2009cAsym_atlas-MMPthal_dseg
+#fslmaths \
+#    space-MNI152NLin2009cAsym_atlas-MMP_dseg \
+#    -add rthalatlas \
+#    space-MNI152NLin2009cAsym_atlas-MMPthal_dseg
 
 
 ## Alternative approach to match templateflow 2mm atlas tpl-MNI152NLin2009cAsym_res-02_T1w.nii.gz
@@ -35,6 +35,7 @@ flirt \
 
 fslstats -K rthalmask rthalmask -m -v
 
+./make_MMP_tsv.py
 ./make_thalamus_voxels.py
 
 flirt \
