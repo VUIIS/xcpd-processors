3ed6e0a1ba2f8f19d5a6d881ec56f2e0fcd473f0
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-07-24 07:12:32 -0500

    csvs bugfixes

--- scripts/stats_csvs.py
+++ scripts/stats_csvs.py
@@ -8,6 +8,7 @@ import os
 import pandas
 import sys
 
+parser = argparse.ArgumentParser()
 parser.add_argument('--xcpd_dir', required=True, 
     help='Absolute path of xcpd output (must match subject/session/etc of fmriprep)')
 parser.add_argument('--atlas', required=True, 
@@ -28,7 +29,8 @@ qc_tsv = bids_xcpd.get(
     )
 if len(qc_tsv)!=1:
     raise Exception(f'Found {len(qc_tsv)} qc .tsv instead of 1')
-qc = qc_tsv[0].get_df()
+qc_tsv = qc_tsv[0]
+qc = qc_tsv.get_df()
 qc.to_csv(os.path.join(args.out_dir, 'qc.csv'), index=False)
 
 # Find coverage file
